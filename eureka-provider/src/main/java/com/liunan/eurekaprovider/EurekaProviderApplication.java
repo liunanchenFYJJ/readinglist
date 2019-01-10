21713e089b3a5b7cb7d71de07a403560961d7a40
LiunanChen <liunan_chen@163.com>
2019-01-10 11:22:18 +0800

    update eureka-provider

--- eureka-provider/src/main/java/com/liunan/eurekaprovider/EurekaProviderApplication.java
+++ eureka-provider/src/main/java/com/liunan/eurekaprovider/EurekaProviderApplication.java
@@ -1,8 +1,11 @@
 package com.liunan.eurekaprovider;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class EurekaProviderApplication {
 
